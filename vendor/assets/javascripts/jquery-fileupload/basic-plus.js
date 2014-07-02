469088427649d6221e5f6a3e9f20dcf82836d0c6
Felix Bünemann <buenemann@louis.info>
2014-07-02 20:14:11 +0200

    Update assets to jQuery File Upload 9.5.7 / 5.40.1
    Also updated the README and all vendored javascripts.

---
+++ vendor/assets/javascripts/jquery-fileupload/basic-plus.js
@@ -0,0 +1,10 @@
+//= require jquery-fileupload/vendor/jquery.ui.widget
+//= require jquery-fileupload/vendor/load-image
+//= require jquery-fileupload/vendor/canvas-to-blob
+//= require jquery-fileupload/jquery.iframe-transport
+//= require jquery-fileupload/jquery.fileupload
+//= require jquery-fileupload/jquery.fileupload-process
+//= require jquery-fileupload/jquery.fileupload-image
+//= require jquery-fileupload/jquery.fileupload-audio
+//= require jquery-fileupload/jquery.fileupload-video
+//= require jquery-fileupload/jquery.fileupload-validate
---
+++ vendor/assets/javascripts/jquery-fileupload/jquery-ui.js
@@ -0,0 +1,11 @@
+//= require jquery-fileupload/vendor/tmpl
+//= require jquery-fileupload/vendor/load-image
+//= require jquery-fileupload/vendor/canvas-to-blob
+//= require jquery-fileupload/jquery.iframe-transport
+//= require jquery-fileupload/jquery.fileupload
+//= require jquery-fileupload/jquery.fileupload-process
+//= require jquery-fileupload/jquery.fileupload-image
+//= require jquery-fileupload/jquery.fileupload-audio
+//= require jquery-fileupload/jquery.fileupload-video
+//= require jquery-fileupload/jquery.fileupload-validate
+//= require jquery-fileupload/jquery.fileupload-jquery-ui
